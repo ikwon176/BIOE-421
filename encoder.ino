d0e66756d9e33eb3101199a48d3337bd66f7fa99
ikwon176 <55472672+ikwon176@users.noreply.github.com>
2021-04-28 00:02:13 -0500

    Update encoder.ino
    Added alphabet in addition to numbers

--- encoder.ino
+++ encoder.ino
@@ -107,6 +107,166 @@ void nine() {
   dot();
 }
 
+void a() {
+  dot();
+  dash();
+}
+
+void b() {
+  dash();
+  dot();
+  dot();
+  dot();
+}
+
+void c() {
+  dash();
+  dot();
+  dash();
+  dot();
+}
+
+void d() {
+  dash();
+  dot();
+  dot();
+}
+
+void e() {
+  dot();
+}
+
+void f() {
+  dot();
+  dot();
+  dash();
+  dot();
+}
+
+void g() {
+  dash();
+  dash();
+  dot();
+}
+
+void h() {
+  dot();
+  dot();
+  dot();
+  dot();
+}
+
+void i() {
+  dot();
+  dot();
+}
+
+void j() {
+  dot();
+  dash();
+  dash();
+  dash();
+}
+
+void k() {
+  dash();
+  dot();
+  dash();
+}
+
+void l() {
+  dot();
+  dash();
+  dot();
+  dot();
+}
+
+void m() {
+  dash();
+  dash();
+}
+
+void n() {
+  dash();
+  dot();
+}
+
+void o() {
+  dash();
+  dash();
+  dash();
+}
+
+void p() {
+  dot();
+  dash();
+  dash();
+  dot();
+}
+
+void q() {
+  dash();
+  dash();
+  dot();
+  dash();
+}
+
+void r() {
+  dot();
+  dash();
+  dot();
+}
+
+void s() {
+  dot();
+  dot();
+  dot();
+}
+
+void t() {
+  dash();
+}
+
+void u() {
+  dot();
+  dot();
+  dash();
+}
+
+void v() {
+  dot();
+  dot();
+  dot();
+  dash();
+}
+
+void w() {
+  dot();
+  dash();
+  dash();
+}
+
+void x() {
+  dash();
+  dot();
+  dot();
+  dash();
+}
+
+void y() {
+  dash();
+  dot();
+  dash();
+  dash();
+}
+
+void z() {
+  dash();
+  dash();
+  dot();
+  dot();
+}
+
 //Encode inputted numbers to morse code numbers and illuminates neopixel appropriately
 void encode() {
   if (input == '0') {
@@ -139,6 +299,84 @@ void encode() {
   else if (input == '9') {
     nine();
   }
+  else if (input == 'a') {
+    a();
+  }
+  else if (input == 'b') {
+    b();
+  }
+  else if (input == 'c') {
+    c();
+  }
+  else if (input == 'd') {
+    d();
+  }
+  else if (input == 'e') {
+    e();
+  }
+  else if (input == 'f') {
+    f();
+  }
+  else if (input == 'g') {
+    g();
+  }
+  else if (input == 'h') {
+    h();
+  }
+  else if (input == 'i') {
+    i();
+  }
+  else if (input == 'j') {
+    j();
+  }
+  else if (input == 'k') {
+    k();
+  }
+  else if (input == 'l') {
+    l();
+  }
+  else if (input == 'm') {
+    m();
+  }
+  else if (input == 'n') {
+    n();
+  }
+  else if (input == 'o') {
+    o();
+  }
+  else if (input == 'p') {
+    p();
+  }
+  else if (input == 'q') {
+    q();
+  }
+  else if (input == 'r') {
+    r();
+  }
+  else if (input == 's') {
+    s();
+  }
+  else if (input == 't') {
+    t();
+  }
+  else if (input == 'u') {
+    u();
+  }
+  else if (input == 'v') {
+    v();
+  }
+  else if (input == 'w') {
+    w();
+  }
+  else if (input == 'x') {
+    x();
+  }
+  else if (input == 'y') {
+    y();
+  }
+  else if (input == 'z') {
+    z();
+  }
   else { //if it is not an integer, returns invalid input
     Serial.print("Invalid input");
   }
@@ -156,9 +394,12 @@ void loop() {
   while (Serial.available()) //check for input in serial monitor
   {
     input = Serial.read();
-    Serial.print("Number received: ");
+    Serial.print("Character received: ");
     Serial.println(input);
     encode(); //encodes each unit separately
+    delay(3000); //wait between two numbers (3 second delay to be clear) 
   }
-  delay(4500); //wait between two numbers (3 second delay to be clear)
+//  input = '6'; //to test without two serial monitors
+//  encode();
+
 }
